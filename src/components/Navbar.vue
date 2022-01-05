9443d5195079433339e66bc66bc2fae2acdde905
andreoliveiraalves96 <andreoliveiraalves96@gmail.com>
2022-01-05 23:37:59 +0000

    Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,56 +1,111 @@
 <template>
   <div>
-    <div class="nav">
-      <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-          <b-navbar-brand href="#">NavBar</b-navbar-brand>
-
-          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-          <b-collapse id="nav-collapse" is-nav>
-            <b-navbar-nav>
-              <b-nav-item href="#">Link</b-nav-item>
-              <b-nav-item href="#" disabled>Disabled</b-nav-item>
-            </b-navbar-nav>
-
-            <!-- Right aligned nav items -->
-            <b-navbar-nav class="ml-auto">
-              <b-nav-form>
-                <b-form-input
-                  size="sm"
-                  class="mr-sm-2"
-                  placeholder="Search"
-                ></b-form-input>
-                <b-button size="sm" class="my-2 my-sm-0" type="submit"
-                  >Search</b-button
-                >
-              </b-nav-form>
-
-              <b-nav-item-dropdown text="Lang" right>
-                <b-dropdown-item href="#">EN</b-dropdown-item>
-                <b-dropdown-item href="#">ES</b-dropdown-item>
-                <b-dropdown-item href="#">RU</b-dropdown-item>
-                <b-dropdown-item href="#">FA</b-dropdown-item>
-              </b-nav-item-dropdown>
-
-              <b-nav-item-dropdown right>
-                <!-- Using 'button-content' slot -->
-                <template #button-content>
-                  <em>User</em>
-                </template>
-                <b-dropdown-item href="#">Profile</b-dropdown-item>
-                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-              </b-nav-item-dropdown>
-            </b-navbar-nav>
-          </b-collapse>
-        </b-navbar>
+    <div class="navbar">
+      <div class="logozone">
+        <img class="logobg" src="../assets/logoshape.svg" alt="logo back" />
+        <img class="logo" src="../assets/logo1.png" alt="emochamp logo" />
       </div>
+      <div class="navlinks">
+        <a class="curretpage" href="#home">HOME</a>
+        <a href="#news">JOGAR</a>
+        <a href="#contact">PERFIL</a>
+        <a href="#about">ESTATISTICAS</a>
+      </div>
+      <button id="btn1"><a href="#login">ENTRAR</a></button>
+      <button id="btn2"><a href="#register">REGISTAR</a></button>
     </div>
   </div>
 </template>
-
 <script>
 export default {};
 </script>
 
-<style></style>
+<style>
+@font-face {
+  font-family: Comfortaa_Bold;
+  src: "../assets/fonts/Comfortaa-Bold.ttf";
+}
+@font-face {
+  font-family: Comfortaa_Regular;
+  src: "../assets/fonts/Comfortaa-Regular.ttf";
+}
+@font-face {
+  font-family: Playfair_Regular;
+  src: "../assets/fonts/PlayfairDisplay-Regular.ttf";
+}
+@font-face {
+  font-family: Playfair_SemiBold;
+  src: "../assets/fonts/PlayfairDisplay-SemiBold.ttf";
+}
+
+.navbar {
+  margin: 0;
+  width: 100%;
+}
+.navbar .logozone .logobg {
+  position: absolute;
+  width: 354.01px;
+  height: 132.26px;
+  left: -31px;
+  top: -12px;
+}
+.navbar .logozone .logo {
+  position: absolute;
+  width: 160px;
+  height: 35.01px;
+  left: 109px;
+  top: 33px;
+}
+.navbar .navlinks {
+  position: absolute;
+  left: 395px;
+  top: 25px;
+  overflow: hidden;
+}
+.navbar .navlinks a {
+  float: left;
+  color: #24305e;
+  text-align: center;
+  padding: 14px 45px;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: Comfortaa_Bold, Playfair_SemiBold, Arial;
+}
+.navbar .navlinks a:hover,
+.navbar #btn1:hover,
+.navbar #btn2:hover {
+  transform: scale(1.1);
+}
+.navbar #btn1 {
+  position: absolute;
+  right: 275px;
+  background-color: #ffffff;
+  border: 3px solid #374785;
+  border-radius: 5px;
+  top: 23px;
+  padding: 10px 30px;
+}
+.navbar #btn1 a {
+  color: #24305e;
+  text-align: center;
+  font-family: Comfortaa_Bold, Playfair_SemiBold, Arial;
+  font-size: 15px;
+  text-decoration: none;
+}
+.navbar #btn2 {
+  position: absolute;
+  right: 100px;
+  background-color: #374785;
+  border: 3px solid #374785;
+  border-radius: 5px;
+  top: 23px;
+  padding: 10px 30px;
+}
+.navbar #btn2 a {
+  color: #ffffff;
+  text-align: center;
+  font-family: Comfortaa_Bold, Playfair_SemiBold, Arial;
+  font-size: 15px;
+  text-decoration: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,17 @@
 <template>
   <div class="home">
-   
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script>
+import Navbar from "../components/Navbar.vue";
+
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 <style scoped></style>
 // eslint-disable-next-line prettier/prettier
