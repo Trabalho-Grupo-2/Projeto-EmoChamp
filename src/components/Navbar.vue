9fafdc81b7bdfc72b277f654e75745031338f0a5
Beatrizlopes98 <40200198@esmad.ipp.pt>
2022-01-07 20:05:02 +0000

    navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,41 +1,63 @@
 <template>
  <div>
-  <b-navbar toggleable="lg" type="dark" variant="info">
-    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  <!-- <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#">
+        <img class="logo" src="../assets/logo1.png" style="width: 160px; height: 35.01px;" alt="emochamp logo" /></b-navbar-brand>
 
     <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="#">Link</b-nav-item>
-        <b-nav-item href="#" disabled>Disabled</b-nav-item>
+        <b-nav-item href="#">HOME</b-nav-item>
+        <b-nav-item href="#">JOGAR</b-nav-item>
+        <b-nav-item href="#">PERFIL</b-nav-item>
+        <b-nav-item href="#">ESTATÍSTICAS</b-nav-item>
       </b-navbar-nav>
 
-      <!-- Right aligned nav items -->
       <b-navbar-nav class="ml-auto">
-        <b-nav-form>
-          <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-          <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-        </b-nav-form>
-
-        <b-nav-item-dropdown text="Lang" right>
-          <b-dropdown-item href="#">EN</b-dropdown-item>
-          <b-dropdown-item href="#">ES</b-dropdown-item>
-          <b-dropdown-item href="#">RU</b-dropdown-item>
-          <b-dropdown-item href="#">FA</b-dropdown-item>
-        </b-nav-item-dropdown>
-
-        <b-nav-item-dropdown right>
-          <!-- Using 'button-content' slot -->
-          <template #button-content>
-            <em>User</em>
-          </template>
-          <b-dropdown-item href="#">Profile</b-dropdown-item>
-          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-        </b-nav-item-dropdown>
-      </b-navbar-nav>
+          <b-button size="sm" class="my-2 my-sm-0">ENTRAR</b-button>
+          <b-button size="sm" class="my-2 my-sm-0">REGISTAR</b-button>
+</b-navbar-nav>
     </b-collapse>
-  </b-navbar>
+  </b-navbar> -->
+  <!-- Navbar -->
+<nav class="navbar navbar-expand-lg bg-link navbar-light ">
+  <!-- Container wrapper -->
+  <div class="container-fluid">
+
+    <!-- Navbar brand -->
+    <a class="navbar-brand" href="#"><img id="logobg" src="../assets/logoshape.svg" alt="logo back" style="margin-top:-12.8px; margin-left:-11.8px;" /><img class="logo" src="../assets/logo1.png" style="width: 160px; height: 35.01px; margin-top:-22px; margin-left:-220px;" alt="emochamp logo" /></a>
+
+    <!-- Collapsible wrapper -->
+    <div class="navbar-collapse" id="navbarSupportedContent" style="margin-left:100px; margin-top:-22px;">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+        <!-- Link -->
+        <li class="nav-item">
+          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">JOGAR</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">PERFIL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">ESTATÍSTICAS</a>
+        </li>
+
+      </ul>
+
+    </div>
+  </div>
+  
+  <button type="button" class="btn btn-outline-primary" id="btnEntrar" style="color:#374785; border-color: #374785; margin-top:-23px;">ENTRAR</button>
+  &nbsp; &nbsp;&nbsp;
+  <button type="button" class="btn btn-primary" id="btnRegistar" style="background-color:#374785; border-color:#374785; margin-top:-23px;">REGISTAR</button>
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</nav>
+<!-- Navbar -->
+
 </div>
 </template>
 
@@ -46,5 +68,17 @@ export default {
 </script>
 
 <style>
+#btnRegistar:hover{
+  background-color: white !important;
+  color: #374785 !important;
 
+}
+#btnEntrar:hover{
+  background-color: #374785 !important;
+  color:  white !important;
+
+}
+.nav-link:hover{
+  transform: scale(1.1) !important;
+}
 </style>
