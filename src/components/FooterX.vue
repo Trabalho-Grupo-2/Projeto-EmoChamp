58bcf3dff6b46f271503ca99bd7db27149dbecf8
andreoliveiraalves96 <andreoliveiraalves96@gmail.com>
2022-01-07 20:23:53 +0000

    asd

--- src/components/FooterX.vue
+++ src/components/FooterX.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="w-100 p-3 text-center" style="background-color: #24305e">
-      <div class="b-row" style="height: 30px"></div>
+      <div class="b-row" style="height: 10px"></div>
       <div class="b-row">
         <img
           src="../assets/logo1.png"
@@ -10,10 +10,10 @@
           style="width: 200px; height: 40px"
         />
       </div>
-      <div class="b-row" style="height: 50px"></div>
+      <div class="b-row" style="height: 40px"></div>
       <div class="b-row">
         <div class="col"></div>
-        <p class="fs6 text-white">
+        <p class="text-white" style="font-size: 13px">
           <img
             src="../assets/Social Icons/location.svg"
             alt="location"
@@ -23,12 +23,12 @@
               filter: invert(98%) sepia(59%) saturate(3404%) hue-rotate(302deg)
                 brightness(128%) contrast(95%);
             "
-          />
+          />&nbsp;&nbsp;
           Rua D. Sancho I n.º 981 4480-876 Vila do Conde
         </p>
       </div>
       <div class="b-row">
-        <p class="fs6 text-white">
+        <p class="text-white" style="font-size: 13px; margin-left: -175px;">
           <img
             src="../assets/Social Icons/contact.svg"
             alt="location"
@@ -38,12 +38,12 @@
               filter: invert(98%) sepia(59%) saturate(3404%) hue-rotate(302deg)
                 brightness(128%) contrast(95%);
             "
-          />
+          />&nbsp;&nbsp;
           +351 252 291 700
         </p>
       </div>
       <div class="b-row">
-        <p class="fs6 text-white">
+        <p class="text-white" style="font-size: 13px ; margin-left: -140px">
           <img
             src="../assets/Social Icons/mail.svg"
             alt="location"
@@ -53,31 +53,31 @@
               filter: invert(98%) sepia(59%) saturate(3404%) hue-rotate(302deg)
                 brightness(128%) contrast(95%);
             "
-          />
+          />&nbsp;&nbsp;
           emochamp@gmail.com
         </p>
       </div>
-      <div class="b-row" style="height: 50px"></div>
+      <div class="b-row" style="height: 30px"></div>
       <div class="b-row">
         <a href="https://www.instagram.com/">
-          <img class="socialImg" src="../assets/social/instagram.svg" />
+          <img class="socialImg" width="30px" src="../assets/social/instagram.svg" />
         </a>
         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
         <a href="https://www.facebook.com/">
-          <img class="socialImg" src="../assets/social/facebook.svg" />
+          <img class="socialImg" width="30px" src="../assets/social/facebook.svg" />
         </a>
         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
         <a href="https://github.com/Trabalho-Grupo-2/Projeto-EmoChamp">
-          <img class="socialImg" src="../assets/social/github.svg" />
+          <img class="socialImg" width="30px" src="../assets/social/github.svg" />
         </a>
         &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
         <a href="https://www.youtube.com/">
-          <img class="socialImg" src="../assets/social/youtube.svg" />
+          <img class="socialImg" width="30px" src="../assets/social/youtube.svg" />
         </a>
       </div>
       <div class="b-row" style="height: 50px"></div>
       <div class="b-row">
-        <p class="fs6 text-white">Copyright © 2021 EmoChamp</p>
+        <p class="text-white" style="font-size: 12px">Copyright © 2021 EmoChamp</p>
       </div>
       <div class="b-row" style="height: 20px"></div>
     </div>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,64 +1,112 @@
 <template>
- <div>
-  <!-- Navbar -->
-<nav class="navbar navbar-expand-lg bg-link navbar-light ">
-  <!-- Container wrapper -->
-  <div class="container-fluid">
+  <div>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-link navbar-light">
+      <!-- Container wrapper -->
+      <div class="container-fluid">
+        <!-- Navbar brand -->
+        <a class="navbar-brand" href="#"
+          ><img
+            id="logobg"
+            src="../assets/logoshape.svg"
+            alt="logo back"
+            style="margin-top: -12.8px; margin-left: -11.8px" /><img
+            class="logo"
+            src="../assets/logo1.png"
+            style="
+              width: 160px;
+              height: 35.01px;
+              margin-top: -22px;
+              margin-left: -220px;
+            "
+            alt="emochamp logo"
+        /></a>
 
-    <!-- Navbar brand -->
-    <a class="navbar-brand" href="#"><img id="logobg" src="../assets/logoshape.svg" alt="logo back" style="margin-top:-12.8px; margin-left:-11.8px;" /><img class="logo" src="../assets/logo1.png" style="width: 160px; height: 35.01px; margin-top:-22px; margin-left:-220px;" alt="emochamp logo" /></a>
+        <!-- Collapsible wrapper -->
+        <div
+          class="navbar-collapse"
+          id="navbarSupportedContent"
+          style="margin-left: 100px; margin-top: -22px"
+        >
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <!-- Link -->
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                style="margin-left: 30px; color: #24305e"
+                href="#"
+                >HOME</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                style="margin-left: 30px; color: #24305e"
+                href="#"
+                >JOGAR</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                style="margin-left: 30px; color: #24305e"
+                href="#"
+                >PERFIL</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                style="margin-left: 30px; color: #24305e"
+                href="#"
+                >ESTATÍSTICAS</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
 
-    <!-- Collapsible wrapper -->
-    <div class="navbar-collapse" id="navbarSupportedContent" style="margin-left:100px; margin-top:-22px;">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-
-        <!-- Link -->
-        <li class="nav-item">
-          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">HOME</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">JOGAR</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">PERFIL</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" style="margin-left:30px; color:#24305e;" href="#">ESTATÍSTICAS</a>
-        </li>
-
-      </ul>
-
-    </div>
+      <button
+        type="button"
+        class="btn btn-outline-primary"
+        id="btnEntrar"
+        style="color: #374785; border-color: #374785; margin-top: -23px"
+      >
+        ENTRAR
+      </button>
+      &nbsp; &nbsp;&nbsp;
+      <button
+        type="button"
+        class="btn btn-primary"
+        id="btnRegistar"
+        style="
+          background-color: #374785;
+          border-color: #374785;
+          margin-top: -23px;
+        "
+      >
+        REGISTAR
+      </button>
+      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    </nav>
+    <!-- Navbar -->
   </div>
-  
-  <button type="button" class="btn btn-outline-primary" id="btnEntrar" style="color:#374785; border-color: #374785; margin-top:-23px;">ENTRAR</button>
-  &nbsp; &nbsp;&nbsp;
-  <button type="button" class="btn btn-primary" id="btnRegistar" style="background-color:#374785; border-color:#374785; margin-top:-23px;">REGISTAR</button>
-  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-</nav>
-<!-- Navbar -->
-
-</div>
 </template>
 
 <script>
-export default {
-    
-}
+export default {};
 </script>
 
 <style>
-#btnRegistar:hover{
+#btnRegistar:hover {
   background-color: white !important;
   color: #374785 !important;
-
 }
-#btnEntrar:hover{
+#btnEntrar:hover {
   background-color: #374785 !important;
-  color:  white !important;
-
+  color: white !important;
 }
-.nav-link:hover{
+.nav-link:hover {
   transform: scale(1.1) !important;
 }
 </style>
