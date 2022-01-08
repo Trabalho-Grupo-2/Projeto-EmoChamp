9094a4bf41c5233b94261f195d9647d300d33f45
Beatrizlopes98 <40200198@esmad.ipp.pt>
2022-01-08 18:19:35 +0000

    registo

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -5,28 +5,28 @@
       id="logobg"
       src="../assets/redelipse.svg"
       alt="logo back"
-      style="margin-top: 60px"
+      style="margin-top: 60px; position: absolute;"
     />
     <div class="w-100 p-3 text-center">
       <div class="b-row">
-        <h1 class="display-4" style="color: #374785; font-size: 30px; margin-top: -300px">
+        <h1 class="display-4" style="color: #374785; font-size: 30px; margin-top: 50px">
           Escolhe o cenário mais adequado à tua situação atual.
         </h1>
       </div>
     </div>
     <section class="container">
       <div class="card">
-        <div class="cardImage card-1"></div>
+        <div class="cardImage"><img class="card-1" src="../assets/Users/human-verde.svg"></div>
         <h2>Estou Sozinho</h2>
         <p>Adequado caso estejas a utilizar   aplicação por conta própria. Caso estejas com dificuldades procura a ajuda de alguém que conheças.</p>
       </div>
       <div class="card">
-        <div class="cardImage card-2"></div>
+        <div class="cardImage"><img class="card-2" src="../assets/Users/human-red.svg"></div>
         <h2>Estou Acompanhado</h2>
         <p>Adequado para utilização  supervisionada. É possível a  criação de filtros e jogos costumizados, permitindo uma resposta mais eficaz face às necessidades que procura.</p>
       </div>
       <div class="card">
-        <div class="cardImage card-3"></div>
+        <div class="cardImage"><img class="card-3" src="../assets/Users/Humaaans Standing.svg"></div>
         <h2>Sou um Psicólogo</h2>
         <p>A aplicação permite gerir um    conjunto de pacientes, fornecendo acesso ao seu desempenho. Obtenha informação e personalize o seu planeamento de consultas.</p>
       </div>
@@ -56,19 +56,36 @@ export default {
 }
 .container {
   display: flex;
-  width: 80%;
+  width:85% !important;
   justify-content: space-around;
+  margin-top: 80px;
+  margin-bottom: 300px;
+  overflow:hidden;
 }
 .card {
   background-color: #F8E9A1 !important;
-  border:none;
+  border:none !important;
   width:269px;
   height:550px;
   color:#374785;
+  text-align: center;
 }
 .cardImage{
-  background-color: #000000;
   height: 283.66px;
   margin-bottom: 15px;
+  background-size: cover;
+}
+.card-2{
+  margin-top: 5px;
+}
+.card-3{
+  margin-top: 6px;
+}
+.card h2{
+  padding: 10px;
+}
+.card p{
+  padding: 10px;
+  font-size: 13px;
 }
 </style>
