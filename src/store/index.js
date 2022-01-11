53220da1d3ee36638885655e539b49a1e6989f74
andreoliveiraalves96 <andreoliveiraalves96@gmail.com>
2022-01-11 19:39:59 +0000

    asd

--- src/store/index.js
+++ src/store/index.js
@@ -6,10 +6,19 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     selectedUser: null,
+    isLogged : false,
+    users: localStorage.getItem("users") ? JSON.parse(localStorage.getItem("users")) : [],
+    form: {
+      name: "",
+      aboutMe: "",
+      password: "",
+      email: "",
+      avatar: ""
+    }
   },
   getters: {
     getselectedUser: state => state.selectedUser
-    
+
   },
   mutations: {
     SETSELECTEDUSER: (state, payload) => { (state.selectedUser = payload); console.log(state.selectedUser)},
