998ac797049d72014d56d5ddb09a8741a24b08a4
Beatrizlopes98 <40200198@esmad.ipp.pt>
2022-01-12 16:24:12 +0000

    routers

--- src/views/Game1.vue
+++ src/views/Game1.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <Navbar />
+    <img id="pagemarker" src="../assets/activepagemarker.svg" alt="marker" />
+    <div class="g1instructions">
+      <h2>Jogo 1 - Reconhecimento de Expressões</h2>
+      <p>Intruções</p>
+      <ol>
+        <li>Na área de jogo podes encontrar 4 imagens.</li>
+        <li>Seleciona a imagem que achas que corresponde à emoção descrita.</li>
+        <li>Continua a adivinhar até que o jogo acabe.</li>
+        <li>Diverte-te!</li>
+      </ol>
+    </div>
     <FooterX />
   </div>
 </template>
@@ -17,4 +28,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+#pagemarker {
+  position: relative;
+  left: 550px;
+  top: -15px;
+  z-index: -1;
+}
+.div {
+  height: 500px;
+}
+</style>
