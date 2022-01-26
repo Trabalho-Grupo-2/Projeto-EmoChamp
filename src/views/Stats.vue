6303485af82289bf655386c4e9614056d93fce53
Beatrizlopes98 <40200198@esmad.ipp.pt>
2022-01-26 01:31:49 +0000

    stats

--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -2,9 +2,19 @@
     <div>
         <Navbar />
 
-        <div>
-
+        <div class="statsImg">
+            <img id="pagemarker" src="../assets/activepagemarker.svg" alt="marker" />
+            <img id="redBaloonTop" src="../assets/redelipse.svg" alt="Red Baloon" style="margin-top: 60px" />
         </div>
+        <div class="statsTab">
+            <b-tabs content-class="mt-3">
+                <b-tab title="Geral" active><p>I'm the first tab</p></b-tab>
+                <b-tab title="Jogo 1"><p>I'm the second tab</p></b-tab>
+                <b-tab title="Jogo 2" active><p>I'm the first tab</p></b-tab>
+                <b-tab title="Categorias"><p>I'm the second tab</p></b-tab>
+            </b-tabs>
+        </div>
+
 
         <FooterX />
     </div>
@@ -22,4 +32,21 @@
     };
 </script>
 
-<style></style>
+<style>
+.statsImg #pagemarker {
+        position: absolute;
+        left: 897px;
+        top: 0px;
+        z-index: -1;
+    }
+
+.statsImg #redBaloonTop {
+    margin-top: 60px;
+    position: absolute;
+}
+.statsTab{
+    width:50%;
+    margin-top: 10%;
+    margin-left: 25%;
+}
+</style>
